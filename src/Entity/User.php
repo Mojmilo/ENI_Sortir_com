515e9db66be5ff7264cf23749bae69f0d804fa5e
Anaïs <anais.mrb@outlook.fr>
2024-11-20 09:02:08 +0100

    Update
    - Ajout du pseudo dans User.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -21,6 +21,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    private ?string $pseudo = null;
+
+    #[ORM\Column(length: 255)]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 255)]
@@ -65,6 +68,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(string $pseudo): static
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
     }
 
     public function getFirstName(): ?string
