ec5b439db6272fa8004f2f0874a4835781c38197
Aurelia <aurelia.attard@gmail.com>
2024-11-26 14:56:43 +0100

    WIP CSV

--- src/Form/CityType.php
+++ src/Form/CityType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\City;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CityType extends AbstractType
@@ -12,8 +14,12 @@ class CityType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('postalCode')
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la ville',
+            ])
+            ->add('postalCode', NumberType::class, [
+                'label' => 'Code postal',
+            ])
         ;
     }
 
