ea8e762dab707150340770116ea2c811d5419cf5
Aurelia <aurelia.attard@gmail.com>
2024-11-26 14:56:43 +0100

    WIP CSV

--- src/Form/SiteType.php
+++ src/Form/SiteType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Site;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SiteType extends AbstractType
@@ -12,7 +13,9 @@ class SiteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'Nom du site',
+            ])
         ;
     }
 
