502232937f92f511e61f7677253449b1cce9f993
Aurelia <aurelia.attard@gmail.com>
2024-11-27 16:39:33 +0100

    15-1006-Import-utilisateurs-par-CSV

--- src/Form/ImportUsersType.php
+++ src/Form/ImportUsersType.php
@@ -16,7 +16,7 @@ class ImportUsersType extends AbstractType
                 'label' => 'Fichier CSV',
                 'constraints' => [
                     new File([
-                        'mimeTypes' => ['text/csv', 'application/csv'],
+                        'mimeTypes' => ['text/csv', 'application/csv', 'application/vnd.ms-excel'],
                         'mimeTypesMessage' => 'Veuillez télécharger un fichier CSV valide.',
                     ])
                 ],
